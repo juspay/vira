2ba97c0347fcb9e7acffdc10970a5f6d260b8a56
Sridhar Ratnakumar <srid@srid.ca>
2025-03-04 13:33:38 -0500

    fix the hang using `tail -f` (still not good)

--- src/Vira/Page/JobLog.hs
+++ src/Vira/Page/JobLog.hs
@@ -5,7 +5,8 @@
 
 module Vira.Page.JobLog where
 
-import Control.Concurrent (threadDelay)
+import Control.Concurrent (forkIO, threadDelay)
+import Control.Concurrent.STM (TChan, newTChanIO, tryReadTChan, writeTChan)
 import Effectful (Eff)
 import Effectful.Error.Static (throwError)
 import Htmx.Lucid.Core (hxSwap_)
@@ -16,7 +17,8 @@ import Servant.API.EventStream (ServerEvent (ServerEvent), ServerSentEvents, ToS
 import Servant.Server.Generic (AsServer)
 import Servant.Types.SourceT qualified as S
 import System.FilePath ((</>))
-import System.IO (hClose, hGetLine, openFile)
+import System.IO (hGetLine)
+import System.Process (CreateProcess (std_out), ProcessHandle, StdStream (CreatePipe), createProcess, proc, terminateProcess)
 import Vira.App qualified as App
 import Vira.App.LinkTo.Type qualified as LinkTo
 import Vira.Lib.HTMX (hxSseConnect_, hxSseSwap_)
@@ -47,7 +49,7 @@ rawLogHandler jobId = do
     liftIO $ readFileBS $ job.jobWorkingDir </> "output.log"
   pure $ decodeUtf8 logText
 
-data LogChunk = LogChunk Int (Maybe Handle) (Html ())
+data LogChunk = LogChunk Int (Maybe TailF) (Html ())
 
 instance ToServerEvent LogChunk where
   toServerEvent (LogChunk ident _handle t) =
@@ -56,37 +58,72 @@ instance ToServerEvent LogChunk where
       (Just $ show ident)
       (Lucid.renderBS t)
 
+-- Represent a `tail -f` process along with its output gathered up in TChat
+data TailF = TailF Int FilePath (TChan Text)
+
+newTailF :: Int -> FilePath -> IO TailF
+newTailF n filePath = do
+  TailF n filePath <$> newTChanIO
+
+runTailF :: TailF -> IO ProcessHandle
+runTailF (TailF n filePath chan) = do
+  -- Run `tail -f` using System.Process and stream its output to chan
+  (_, Just hOut, _, ph) <- createProcess (proc "tail" ["-n", show n, "-f", filePath]) {std_out = CreatePipe}
+  hSetBuffering hOut LineBuffering
+  -- hSeek hOut AbsoluteSeek 0
+  void $ forkIO $ do
+    let loop = do
+          hIsEOF hOut >>= \case
+            True -> pass
+            False -> do
+              line <- hGetLine hOut
+              atomically $ writeTChan chan $ toText line
+              loop
+    loop
+  pure ph
+
+tryReadTailF :: TailF -> IO (Maybe Text)
+tryReadTailF (TailF _ _ chan) = atomically $ tryReadTChan chan
+
 streamHandler :: App.AppState -> JobId -> S.SourceT IO LogChunk
 streamHandler cfg jobId = S.fromStepT $ step 0 Nothing
   where
-    step (n :: Int) (mh :: Maybe Handle) = S.Effect $ do
+    step (n :: Int) (mh :: Maybe (TailF, ProcessHandle)) = S.Effect $ do
       job :: Job <-
         App.runApp cfg $
           App.query (St.GetJobA jobId)
             >>= maybe (error "Job not found") pure
       let jobActive = job.jobStatus == St.JobRunning || job.jobStatus == St.JobPending
       let logFile = job.jobWorkingDir </> "output.log"
-      h <- maybe (liftIO $ openFile logFile ReadMode) pure mh
+      (h, p) <-
+        maybe
+          ( liftIO $ do
+              t <- newTailF 1000 logFile
+              p <- runTailF t
+              pure (t, p)
+          )
+          pure
+          mh
       -- TODO:
       -- 1. Read last N lines, rather than reading from scratch
       -- 2. Send chunks, not single line. And delay?
-      eof <- liftIO $ hIsEOF h
-      case (eof, jobActive) of
-        (True, True) -> do
+      mLine <- tryReadTailF h
+      case (mLine, jobActive) of
+        (Nothing, True) -> do
           threadDelay 100_000
-          pure $ S.Skip $ step (n + 1) (Just h)
-        (True, False) -> do
-          liftIO $ hClose h
+          pure $ S.Skip $ step (n + 1) (Just (h, p))
+        (Nothing, False) -> do
+          -- TODO: Drain all of tail -f's output first
+          terminateProcess p
           -- FIXME:
           -- pure S.Stop
           putStrLn "!!!!!! Due to sse bug, waiting infinitely"
           forever $ do
             threadDelay 1_000_000_000
             pure S.Stop
-        (False, _) -> do
-          chunk <- liftIO $ hGetLine h
-          let msg = LogChunk n (Just h) (pre_ $ toHtml chunk)
-          pure $ S.Yield msg $ step (n + 1) (Just h)
+        (Just line, _) -> do
+          let msg = LogChunk n (Just h) (pre_ $ toHtml line)
+          pure $ S.Yield msg $ step (n + 1) (Just (h, p))
 
 viewStream :: (LinkTo.LinkTo -> Link) -> St.Job -> Html ()
 viewStream linkTo job = do
@@ -96,14 +133,13 @@ viewStream linkTo job = do
         a_
           [target_ "blank", class_ "underline text-blue-500", href_ $ show . linkURI $ linkTo $ LinkTo.JobLog job.jobId]
           "View Full Log"
-    pre_ [class_ "bg-black text-white p-2 text-xs"] $ code_ $ do
-      let streamLink = show . linkURI $ linkTo $ LinkTo.JobLogStream job.jobId
-      div_
-        [ hxExt_ "sse"
-        , hxSseConnect_ streamLink
-        , hxSwap_ "beforeend"
-        , hxSseSwap_ "logchunk"
-        ]
-        $ do
-          "Loading log ..."
+    let streamLink = show . linkURI $ linkTo $ LinkTo.JobLogStream job.jobId
+    let sseAttrs =
+          [ hxExt_ "sse"
+          , hxSseConnect_ streamLink
+          , hxSwap_ "beforeend"
+          , hxSseSwap_ "logchunk"
+          ]
+    pre_ (sseAttrs <> [class_ "bg-black text-white p-2 text-xs", style_ "white-space: pre-wrap;"]) $ code_ $ do
+      "Loading log ..."
     div_ "TODO: add running or finished indicator"
