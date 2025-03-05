0a63e1c35232ef3f0166d9344ce72ce720029c93
Sridhar Ratnakumar <srid@srid.ca>
2025-03-05 16:13:12 -0500

    refactor

--- src/Vira/Page/JobLog.hs
+++ src/Vira/Page/JobLog.hs
@@ -49,52 +49,66 @@ data LogChunk
   = Chunk Int (Html ())
   | Stop Int
 
+logChunkType :: LogChunk -> Text
+logChunkType = \case
+  Chunk _ _ -> "logchunk"
+  Stop _ -> "logstop"
+
+logChunkId :: LogChunk -> LByteString
+logChunkId = \case
+  Chunk ident _ -> show ident
+  Stop ident -> show ident
+
+logChunkMsg :: LogChunk -> LByteString
+logChunkMsg = \case
+  Chunk _ html -> Lucid.renderBS html
+  Stop _ -> "Log stopped"
+
 instance ToServerEvent LogChunk where
-  toServerEvent = \case
-    Chunk ident t ->
-      ServerEvent
-        (Just "logchunk")
-        (Just $ show ident)
-        (Lucid.renderBS t)
-    Stop ident ->
-      ServerEvent
-        (Just "logstop")
-        (Just $ show ident)
-        (Lucid.renderBS "Log stopped")
+  toServerEvent chunk =
+    ServerEvent
+      (Just $ encodeUtf8 $ logChunkType chunk)
+      (Just $ logChunkId chunk)
+      (logChunkMsg chunk)
+
+data StreamState = Init | Streaming TailF | Stopping
 
 streamHandler :: App.AppState -> JobId -> S.SourceT IO LogChunk
-streamHandler cfg jobId = S.fromStepT $ step 0 Nothing
+streamHandler cfg jobId = S.fromStepT $ step 0 Init
   where
-    step (n :: Int) (mh :: Maybe TailF) = S.Effect $ do
+    step (n :: Int) (st :: StreamState) = S.Effect $ do
       job :: Job <-
         App.runApp cfg $
           App.query (St.GetJobA jobId)
             >>= maybe (error "Job not found") pure
-      let jobActive = job.jobStatus == St.JobRunning || job.jobStatus == St.JobPending
       let logFile = job.jobWorkingDir </> "output.log"
-      logTail <-
-        maybe (liftIO $ TailF.new logFile) pure mh
-      -- TODO:
-      -- 1. Read last N lines, rather than reading from scratch
-      -- 2. Send chunks, not single line. And delay?
+      case st of
+        Init -> do
+          logTail <- liftIO $ TailF.new logFile
+          streamLog n job logTail
+        Streaming logTail -> do
+          streamLog n job logTail
+        Stopping -> do
+          -- Keep going unless client has time to catch up.
+          threadDelay 1000_000
+          pure $ S.Yield (Stop n) $ step (n + 1) st
+    streamLog n job logTail = do
+      let jobActive = job.jobStatus == St.JobRunning || job.jobStatus == St.JobPending
       mLine <- TailF.tryRead logTail
       case (mLine, jobActive) of
         (Nothing, True) -> do
           -- Job is active, but log yet to be updated; retry.
           threadDelay 100_000
-          pure $ S.Skip $ step (n + 1) (Just logTail)
+          pure $ S.Skip $ step n (Streaming logTail)
         (Nothing, False) -> do
           -- Job ended
           -- TODO: Drain all of tail -f's output first
           putStrLn "Job ended; ending stream"
           TailF.stop logTail
-          -- This delay is required lest we are called again.
-          threadDelay 1000_000
-          -- FIXME: Why poll after this?
-          pure $ S.Yield (Stop n) $ step (n + 1) (Just logTail)
+          pure $ S.Yield (Stop n) $ step (n + 1) Stopping
         (Just line, _) -> do
           let msg = Chunk n (pre_ $ toHtml line)
-          pure $ S.Yield msg $ step (n + 1) (Just logTail)
+          pure $ S.Yield msg $ step (n + 1) (Streaming logTail)
 
 viewStream :: (LinkTo.LinkTo -> Link) -> St.Job -> Html ()
 viewStream linkTo job = do
@@ -109,8 +123,8 @@ viewStream linkTo job = do
           [ hxExt_ "sse"
           , hxSseConnect_ streamLink
           , hxSwap_ "beforeend show:window:bottom"
-          , hxSseSwap_ "logchunk"
-          , hxSseClose_ "logstop"
+          , hxSseSwap_ $ logChunkType $ Chunk 0 mempty
+          , hxSseClose_ $ logChunkType $ Stop 0
           ]
     pre_ (sseAttrs <> [class_ "bg-black text-white p-2 text-xs", style_ "white-space: pre-wrap;"]) $ code_ $ do
       "Loading log ..."
