4f853a170fd56a04a79bbea1f25e4206958880c2
Sridhar Ratnakumar <srid@srid.ca>
2025-03-13 10:51:14 -0400

    haskell: Switch to optparse-applicative
    We are not using any of the features that warrant this new library. Use
    the tried and tested optparse-applicative. For generating NixOS module
    generation, let's come up with a decoupled solution.

--- src/Vira/App/CLI.hs
+++ src/Vira/App/CLI.hs
@@ -1,12 +1,24 @@
 {-# LANGUAGE ApplicativeDo #-}
 {-# LANGUAGE RecordWildCards #-}
 
-module Vira.App.CLI where
+module Vira.App.CLI (
+  -- * Types
+  Settings (..),
+  RepoSettings (..),
+  CachixSettings (..),
+
+  -- * Function
+  parseCLI,
+) where
 
 import Data.Set qualified as Set
+import Data.Text qualified as T
+import Data.Version (showVersion)
+import Network.HostName (HostName, getHostName)
 import Network.Wai.Handler.Warp (Port)
-import OptEnvConf
-import Prelude hiding (Reader, reader)
+import Options.Applicative
+import Paths_vira qualified
+import Prelude hiding (Reader, reader, runReader)
 
 {- | CLI settings
 TODO: Use Severity from co-log
@@ -47,98 +59,6 @@ data CachixSettings = CachixSettings
   }
   deriving stock (Show)
 
-instance HasParser Settings where
-  settingsParser = withoutConfig $ do
-    logLevel <-
-      setting
-        [ reader str
-        , metavar "LOG_LEVEL"
-        , help "Log level"
-        , name "log-level"
-        , value "Debug"
-        ]
-    port <-
-      setting
-        [ reader auto
-        , metavar "PORT"
-        , help "Port to bind the HTTP server to"
-        , name "port"
-        , value 5005
-        ]
-    host <-
-      setting
-        [ reader str
-        , metavar "HOST"
-        , help "Host"
-        , name "host"
-        , value "127.0.0.1"
-        ]
-    dbPath <-
-      setting
-        [ reader str
-        , metavar "DB_PATH"
-        , help "Path to vira db"
-        , name "db-path"
-        , value "vira.db"
-        ]
-    repo <- subSettings "repo"
-    instanceName <-
-      setting
-        [ reader str
-        , metavar "INSTANCE_NAME"
-        , help "Name of the instance; uses hostname if unspecified"
-        , name "instance-name"
-        , value ""
-        ]
-    basePath <-
-      setting
-        [ reader str
-        , metavar "BASE_PATH"
-        , help "Base URL path for the http server"
-        , name "base-path"
-        , value "/"
-        ]
-    pure Settings {..}
-
-instance HasParser RepoSettings where
-  settingsParser = withoutConfig $ do
-    cloneUrls <-
-      setting
-        [ reader (commaSeparatedList $ fmap (toText @String) str)
-        , metavar "CLONE_URLS"
-        , help "Repositories (git clone URL) to watch and build"
-        , name "clone-urls"
-        , value defaultRepos
-        ]
-    branchWhitelist <-
-      setting
-        [ reader (Set.fromList <$> commaSeparatedList str)
-        , metavar "BRANCH_WHITELIST"
-        , help "Limit to building these branches"
-        , name "branch-whitelist"
-        , value defaultBranchesToBuild
-        ]
-    cachix <- optional $ subSettings "cachix"
-    pure RepoSettings {..}
-
-instance HasParser CachixSettings where
-  settingsParser = withoutConfig $ do
-    cachixName <-
-      setting
-        [ reader str
-        , metavar "CACHIX_NAME"
-        , help "Name of the cachix cache"
-        , name "name"
-        ]
-    authToken <-
-      setting
-        [ reader str
-        , metavar "CACHIX_AUTH_TOKEN"
-        , help "Auth token for the cachix cache"
-        , name "auth-token"
-        ]
-    pure CachixSettings {..}
-
 defaultRepos :: [Text]
 defaultRepos =
   [ "https://github.com/srid/emanote.git"
@@ -152,3 +72,123 @@ defaultRepos =
 
 defaultBranchesToBuild :: Set Text
 defaultBranchesToBuild = Set.fromList ["main", "master", "staging", "develop", "trunk"]
+
+-- | Parser for Settings
+settingsParser :: HostName -> Parser Settings
+settingsParser hostName = do
+  logLevel <-
+    strOption
+      ( long "log-level"
+          <> metavar "LOG_LEVEL"
+          <> help "Log level"
+          <> value "Debug"
+      )
+  port <-
+    option
+      auto
+      ( long "port"
+          <> metavar "PORT"
+          <> help "Port to bind the HTTP server to"
+          <> value 5005
+      )
+  host <-
+    strOption
+      ( long "host"
+          <> metavar "HOST"
+          <> help "Host"
+          <> value "127.0.0.1"
+          <> showDefault
+      )
+  dbPath <-
+    strOption
+      ( long "db-path"
+          <> metavar "DB_PATH"
+          <> help "Path to vira db"
+          <> value "vira.db"
+          <> showDefault
+      )
+  repo <- repoSettingsParser
+  instanceName <-
+    strOption
+      ( long "instance-name"
+          <> metavar "INSTANCE_NAME"
+          <> help "Name of the instance"
+          <> value (toText hostName)
+          <> showDefault
+      )
+  basePath <-
+    strOption
+      ( long "base-path"
+          <> metavar "BASE_PATH"
+          <> help "Base URL path for the http server"
+          <> value "/"
+          <> showDefault
+      )
+  pure Settings {..}
+
+-- | Parser for RepoSettings
+repoSettingsParser :: Parser RepoSettings
+repoSettingsParser = do
+  cloneUrls <-
+    option
+      commaSeparatedTextList
+      ( long "clone-urls"
+          <> metavar "CLONE_URLS"
+          <> help "Repositories to watch and build, as comma-separated `git clone` URLs"
+          <> value defaultRepos
+          <> showDefault
+      )
+  branchWhitelist <-
+    option
+      (Set.fromList <$> commaSeparatedTextList)
+      ( long "branch-whitelist"
+          <> metavar "BRANCH_WHITELIST"
+          <> help "Limit to building these branches (comma-separated list)"
+          <> value defaultBranchesToBuild
+          <> showDefault
+      )
+  cachix <- optional cachixSettingsParser
+  pure RepoSettings {..}
+
+-- | Parser for CachixSettings
+cachixSettingsParser :: Parser CachixSettings
+cachixSettingsParser = do
+  cachixName <-
+    strOption
+      ( long "cachix-name"
+          <> metavar "CACHIX_NAME"
+          <> help "Name of the cachix cache"
+      )
+  authToken <-
+    strOption
+      ( long "cachix-auth-token"
+          <> metavar "CACHIX_AUTH_TOKEN"
+          <> help "Auth token for the cachix cache"
+      )
+  pure CachixSettings {..}
+
+-- | Full parser with info
+parseSettings :: HostName -> ParserInfo Settings
+parseSettings hostName =
+  info
+    (versionOption <*> settingsParser hostName <**> helper)
+    ( fullDesc
+        <> progDesc "Vira"
+        <> header "vira - Nix CI for teams"
+    )
+  where
+    versionOption =
+      infoOption
+        (showVersion Paths_vira.version)
+        (long "version" <> help "Show version")
+
+-- | Helper to parse comma-separated lists into Text values
+commaSeparatedTextList :: ReadM [Text]
+commaSeparatedTextList = eitherReader $ \s -> do
+  let split = filter (not . T.null) . T.splitOn "," . toText
+  pure (split s)
+
+parseCLI :: IO Settings
+parseCLI = do
+  hostName <- liftIO getHostName
+  execParser $ parseSettings hostName
