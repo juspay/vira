3e3179642aa44252ce76dcf10e4fe88b9b02503d
Shivaraj B H <me@shivaraj-bh.in>
2025-05-20 05:00:38 +0000

    Use `SIGINT` to close Vira gracefully during `just run` shutdown (#31)

--- nix/modules/flake-parts/haskell.nix
+++ nix/modules/flake-parts/haskell.nix
@@ -94,6 +94,8 @@
                   --setup ":set args --cachix-name scratch-vira-dev --cachix-auth-token eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOiI5NDI4ZjhkZi1mZWM5LTQ1ZjctYjMzYi01MTFiZTljNTNkNjciLCJzY29wZXMiOiJjYWNoZSJ9.WgPWUSYIie2rUdfuPqHS5mxrkT0lc7KIN7QPBPH4H-U --base-path ''${BASE_PATH:-/}"
             '';
             depends_on.tailwind.condition = "process_started";
+            # Without `SIGINT (2)` Vira doesn't close gracefully
+            shutdown.signal = 2;
           };
           tailwind =
             let
