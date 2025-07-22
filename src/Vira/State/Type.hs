9ce9ccb2e9f8da33760c54dcaedf4c50250c610e
shivaraj-bh <me@shivaraj-bh.in>
2025-07-22 16:27:46 +0530

    doesn't have to be in TH

--- src/Vira/State/Type.hs
+++ src/Vira/State/Type.hs
@@ -40,7 +40,7 @@ data AtticSettings = AtticSettings
   }
   deriving stock (Show, Generic)
 
--- TODO: Use TH to type-check the instance definition in compile-time
+-- TODO: type-check field names during compile-time
 instance FromForm AtticSettings where
   fromForm f =
     AtticSettings
@@ -80,7 +80,7 @@ data Repo = Repo
   }
   deriving stock (Generic, Show, Typeable, Data, Eq, Ord)
 
--- TODO: Use TH to type-check the instance definition in compile-time
+-- TODO: type-check field names during compile-time
 instance FromForm Repo where
   fromForm f =
     Repo
