dcb7e0f8f8e0f147b1d81673178c6f04a7f11928
Sridhar Ratnakumar <srid@srid.ca>
2025-08-30 21:15:02 -0400

    monad

--- packages/vira/src/Vira/Widgets/Layout.hs
+++ packages/vira/src/Vira/Widgets/Layout.hs
@@ -133,7 +133,7 @@ breadcrumbs rs' = do
             , class_ "text-white/90 hover:text-white transition-smooth px-3 py-2 rounded-lg hover:bg-white/10 focus:outline-none focus:ring-2 focus:ring-white/30 font-medium"
             ]
             $ toHtml title
-    chevronSvg :: VHtml ()
+    chevronSvg :: (Monad m) => HtmlT m ()
     chevronSvg =
       span_ [class_ "mx-1 text-white/60"] $ toHtmlRaw ("<svg xmlns='http://www.w3.org/2000/svg' class='h-5 w-5' fill='none' viewBox='0 0 24 24' stroke='currentColor' stroke-width='2'><path stroke-linecap='round' stroke-linejoin='round' d='M9 5l7 7-7 7'/></svg>" :: Text)
 
