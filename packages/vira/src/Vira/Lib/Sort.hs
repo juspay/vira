8f037e8a0bfd8eb3e234af0f66b59f6e9cce5cdf
shivaraj-bh <me@shivaraj-bh.in>
2025-09-02 12:46:14 +0530

    `Vira.Lib.Sort`: Add module description

--- packages/vira/src/Vira/Lib/Sort.hs
+++ packages/vira/src/Vira/Lib/Sort.hs
@@ -1,3 +1,7 @@
+{- | Module for defining sort utilities
+
+If more requirements arise, prefer using https://hackage.haskell.org/package/sort instead
+-}
 module Vira.Lib.Sort where
 
 -- | Remove duplicates and sort the list through the mapped list on the given function
