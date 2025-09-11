3776b12e6dcf4930436d61c34a9d962c1ae21a17
Sridhar Ratnakumar <srid@srid.ca>
2025-09-11 16:40:41 -0400

    restore nixSystem

--- packages/gh-signoff/src/System/GHSignoff.hs
+++ packages/gh-signoff/src/System/GHSignoff.hs
@@ -4,8 +4,9 @@
 
 A standalone library for gh-signoff operations.
 -}
-module System.GHSignoff where
+module System.GHSignoff (ghSignoffProcess) where
 
+import System.Info qualified as SysInfo
 import System.Process (CreateProcess, proc)
 import System.Which (staticWhich)
 
@@ -16,6 +17,14 @@ This should be available in the PATH, thanks to Nix and `which` library.
 ghSignoffBin :: FilePath
 ghSignoffBin = $(staticWhich "gh-signoff")
 
--- | Create a process to run gh-signoff create with force flag
+{- | System platform string
+
+Computed from System.Info at runtime in Nix format (arch-os).
+-}
+nixSystem :: String
+nixSystem = SysInfo.arch <> "-" <> SysInfo.os
+
+-- | Create a process to run gh-signoff create with force flag and platform context
 ghSignoffProcess :: String -> String -> CreateProcess
-ghSignoffProcess name k = proc ghSignoffBin ["create", "-f", name <> "/" <> k]
+ghSignoffProcess name k =
+  proc ghSignoffBin ["create", "-f", name <> "/" <> nixSystem <> "/" <> k]
