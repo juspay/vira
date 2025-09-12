d44efea8a0efa05bf2953ea35c06bf2c78c0a125
Sridhar Ratnakumar <srid@srid.ca>
2025-09-12 11:39:00 -0400

    fin

--- docs/flake.nix
+++ docs/flake.nix
@@ -16,7 +16,6 @@
           sites."default" = {
             layers = [{ path = ./.; pathString = "."; }];
             # port = 8080;
-            # baseUrl = "/emanote-template/"; # Change to "/" (or remove it entirely) if using CNAME
             extraConfig = {
               template = {
                 urlStrategy = "pretty";
