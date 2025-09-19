88fcd76974c700ab0e3b51da00558c9fdf1c266c
Sridhar Ratnakumar <srid@srid.ca>
2025-09-19 09:49:15 -0400

    Delete whole data dir
    We don't want to force the user to think in terms of state's sub
    directories.

--- packages/vira/src/Vira/State/Core.hs
+++ packages/vira/src/Vira/State/Core.hs
@@ -28,15 +28,14 @@ openViraState stateDir = do
   -- Manually construct the path that openLocalState would use: stateDir </> show (typeOf initialState)
   -- This is just for backwards compat.
   let acidStateDir = stateDir </> show (typeOf initialState)
-  st <- openLocalStateFrom acidStateDir initialState `catch` handleStateError acidStateDir
+  st <- openLocalStateFrom acidStateDir initialState `catch` handleStateError
   update st MarkUnfinishedJobsAsStaleA
   pure st
   where
-    handleStateError acidStateDir (ErrorCall msg) = do
-      let workspaceDir = stateDir </> "workspace"
+    handleStateError (ErrorCall msg) = do
       putStrLn "ERROR: Failed to open acid-state database. This usually indicates incompatible state format."
-      putStrLn "Please remove the state directory and restart:"
-      putStrLn ("  rm -rf " <> acidStateDir <> " " <> workspaceDir)
+      putStrLn "Please remove the vira data directory and restart:"
+      putStrLn ("  rm -rf " <> stateDir)
       putStrLn "Your data will be lost, but this is necessary to continue."
       putStrLn ""
       putStrLn "Original error:"
