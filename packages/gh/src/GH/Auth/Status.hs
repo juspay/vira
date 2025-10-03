3f9807551ba4edebb3296f474156131160718e4e
Sridhar Ratnakumar <srid@srid.ca>
2025-10-03 15:47:42 -0400

    sum type

--- packages/gh/src/GH/Auth/Status.hs
+++ packages/gh/src/GH/Auth/Status.hs
@@ -8,11 +8,13 @@ Provides functionality to check if the user is authenticated with GitHub CLI.
 -}
 module GH.Auth.Status (
   AuthStatus (..),
+  AuthEntryState (..),
   checkAuthStatus,
 ) where
 
-import Data.Aeson (FromJSON (..), (.:))
+import Data.Aeson (FromJSON (..), Options (..), SumEncoding (UntaggedValue), defaultOptions, genericParseJSON)
 import Data.Aeson qualified as Aeson
+import Data.Char (toLower)
 import Data.Map.Strict qualified as Map
 import Data.Text qualified as T
 import GH.Core (ghBin)
@@ -28,6 +30,22 @@ data AuthStatus
   | NotAuthenticated
   deriving stock (Show, Eq)
 
+-- | Authentication entry state from gh CLI
+data AuthEntryState
+  = Success
+  | Timeout
+  | Error
+  deriving stock (Show, Eq, Generic)
+
+instance FromJSON AuthEntryState where
+  parseJSON = genericParseJSON opts
+    where
+      opts =
+        defaultOptions
+          { constructorTagModifier = map toLower
+          , sumEncoding = UntaggedValue
+          }
+
 -- | JSON response structure from gh auth status
 newtype AuthResponse = AuthResponse
   { hosts :: Map Text [HostAuth]
@@ -36,41 +54,37 @@ newtype AuthResponse = AuthResponse
   deriving anyclass (FromJSON)
 
 data HostAuth = HostAuth
-  { state :: Text
+  { state :: AuthEntryState
+  , error :: Maybe Text
   , active :: Bool
   , host :: Text
   , login :: Text
-  , scopesRaw :: Text
+  , tokenSource :: Text
+  , token :: Maybe Text
+  , scopes :: Maybe Text
+  , gitProtocol :: Text
   }
   deriving stock (Show, Generic)
-
-instance FromJSON HostAuth where
-  parseJSON = Aeson.withObject "HostAuth" $ \v ->
-    HostAuth
-      <$> v .: "state"
-      <*> v .: "active"
-      <*> v .: "host"
-      <*> v .: "login"
-      <*> v .: "scopes"
+  deriving anyclass (FromJSON)
 
 -- | Check GitHub CLI authentication status
 checkAuthStatus :: IO AuthStatus
 checkAuthStatus = do
   output <- readProcess ghBin ["auth", "status", "--json", "hosts"] ""
 
-  case Aeson.decode (encodeUtf8 $ toText output) of
+  case Aeson.decode (encodeUtf8 output) of
     Nothing -> pure NotAuthenticated
     Just (AuthResponse hostsMap) -> do
       case Map.lookup "github.com" hostsMap of
         Nothing -> pure NotAuthenticated
         Just auths -> do
           -- Find active auth with success state
-          case find (\auth -> auth.active && auth.state == "success" && auth.host == "github.com") auths of
+          case find (\auth -> auth.active && auth.state == Success && auth.host == "github.com") auths of
             Nothing -> pure NotAuthenticated
             Just auth ->
               pure $
                 Authenticated
                   { host = auth.host
                   , login = auth.login
-                  , scopes = map T.strip $ T.splitOn "," auth.scopesRaw
+                  , scopes = maybe [] (map T.strip . T.splitOn ",") auth.scopes
                   }
