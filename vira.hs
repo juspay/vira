e7535e491f0e123d5dbe4a87b76feb74c999357f
Sridhar Ratnakumar <srid@srid.ca>
2025-10-04 10:56:36 -0400

    case?

--- vira.hs
+++ vira.hs
@@ -1,11 +1,11 @@
 -- Pipeline configuration for Vira
 \ctx pipeline ->
-  let isMain = ctx.branch == "main"
+  let cacheUrl = case ctx.branch == "main" of
+        True -> Just "https://cache.nixos.asia/oss"
+        False -> Nothing
   in pipeline
     { signoff.enable = True
     , cachix.enable = False
     , attic.enable = False
-    , cache.url = if isMain
-        then Just "https://cache.nixos.asia/oss"
-        else Nothing
+    , cache.url = cacheUrl
     }
