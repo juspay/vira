300873b97fcb2ab4d1ce9b3664147c083f111437
Sridhar Ratnakumar <srid@srid.ca>
2025-10-03 22:12:06 -0400

    Add user information to UI

--- packages/vira/src/Vira/Page/ToolsPage/Handler.hs
+++ packages/vira/src/Vira/Page/ToolsPage/Handler.hs
@@ -14,8 +14,10 @@ import Vira.App qualified as App
 import Vira.App.CLI (WebSettings)
 import Vira.App.LinkTo.Type qualified as LinkTo
 import Vira.Page.ToolsPage.Tool qualified as Tool
+import Vira.Page.ToolsPage.User qualified as User
 import Vira.Page.ToolsPage.View qualified as ToolView
 import Vira.Widgets.Layout qualified as W
+import Web.TablerIcons.Outline qualified as Icon
 
 newtype Routes mode = Routes
   { _view :: mode :- Get '[HTML] (Html ())
@@ -37,8 +39,11 @@ viewTools = do
   tools <- lift $ liftIO Tool.readAllTools
 
   W.viraSection_ [] $ do
-    W.viraPageHeader_ "Tools" $ do
-      p_ [class_ "text-gray-600"] "Command-line tools used by Vira jobs"
+    W.viraPageHeaderWithIcon_ (toHtmlRaw Icon.tool) "Tools" $ do
+      div_ [class_ "flex items-center justify-between"] $ do
+        p_ [class_ "text-gray-600"] "Command-line tools used by Vira jobs"
+        span_ [class_ "text-indigo-800 font-semibold"] User.viewUserInfo
+
     div_ [class_ "grid gap-6 md:grid-cols-2 lg:grid-cols-2"] $ do
       forM_ tools $ \(tool :=> toolData) ->
         ToolView.viewTool tool toolData
