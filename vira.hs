e551bf674081db58fd80893ba8fa076fc0a713dd
Sridhar Ratnakumar <srid@srid.ca>
2025-10-05 20:29:04 -0400

    fin

--- vira.hs
+++ vira.hs
@@ -4,7 +4,7 @@
     isMain = ctx.branch == "main"
     cacheUrl = case isMain of
         True -> Just "https://cache.nixos.asia/oss"
-        False -> Just "https://cache.nixos.asia/oss"
+        False -> Nothing
   in pipeline
     { signoff.enable = True
     , cache.url = cacheUrl
