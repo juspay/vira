1d1cf05d22f785a3c9a3d9fb56dfa2f7ce6ab59d
Sridhar Ratnakumar <3998+srid@users.noreply.github.com>
2025-10-17 15:53:21 -0400

    fix?

--- packages/devour-flake/src/DevourFlake.hs
+++ packages/devour-flake/src/DevourFlake.hs
@@ -27,26 +27,20 @@ data DevourFlakeArgs = DevourFlakeArgs
 Roughly corresponds to:
 nix build github:srid/devour-flake -L --no-link --print-out-paths --override-input flake <flakePath>
 -}
-devourFlake :: DevourFlakeArgs -> [String]
+devourFlake :: (HasCallStack) => DevourFlakeArgs -> [String]
 devourFlake args =
   [ "build"
   , devourFlakePath <> "#json"
   , "-L"
   , "--print-out-paths"
-  , -- To suppress 'override input' verbosity and lock file warnings
-    -- Disabling it doesn't have effect on CI logs
-    -- "--quiet"
-    -- "--quiet"
-    "--no-write-lock-file"
+  , "--no-write-lock-file"
   , "--override-input"
   , "flake"
   , args.flakePath
-  , "--override-input"
-  , "systems"
   ]
     <> maybe ["--no-link"] (\link -> ["--out-link", link]) args.outLink
     <> concatMap (\(k, v) -> ["--override-input", "flake/" <> toString k, toString v]) args.overrideInputs
     <> case args.systems of
       Nothing -> []
       Just _systemsList ->
-        [] -- TODO
+        error "Not implemented"
