661e6b8d025801c89a97e0a2205d03e59eb03b6d
Sridhar Ratnakumar <3998+srid@users.noreply.github.com>
2025-10-17 15:26:37 -0400

    fl

--- packages/devour-flake/src/DevourFlake.hs
+++ packages/devour-flake/src/DevourFlake.hs
@@ -1,26 +1,38 @@
+{-# LANGUAGE OverloadedRecordDot #-}
+
 module DevourFlake (
   devourFlake,
 ) where
 
 import DevourFlake.DevourFlakePath (devourFlakePath)
+import DevourFlake.NixSystems (nixSystemsFlakeFor)
 import System.Nix.System (System (..))
+
+data DevourFlakeArgs = DevourFlakeArgs
+  { system :: System
+  , outLink :: Maybe FilePath
+  , flakePath :: FilePath
+  }
+  deriving stock (Eq, Show)
 
 {- | Generate arguments to pass to CreateProcess for devour-flake
 Roughly corresponds to:
 nix build github:srid/devour-flake -L --no-link --print-out-paths --override-input flake <flakePath>
 -}
-devourFlake :: System -> FilePath -> [String]
-devourFlake system flakePath =
+devourFlake :: DevourFlakeArgs -> [String]
+devourFlake args =
   [ "build"
   , devourFlakePath
   , "-L"
-  , "--no-link"
   , "--print-out-paths"
-  , "--quiet"
+  , -- To suppress 'override input' verbosity
+    "--quiet"
   , "--quiet"
   , "--override-input"
   , "flake"
-  , flakePath
-  , "--system"
-  , toString system
+  , args.flakePath
+  , "--override-input"
+  , "systems"
+  , nixSystemsFlakeFor args.system
   ]
+    <> maybe ["--no-link"] (\link -> ["--out-link", link]) args.outLink
--- packages/devour-flake/src/DevourFlake/NixSystems.hs
+++ packages/devour-flake/src/DevourFlake/NixSystems.hs
@@ -1,12 +1,18 @@
 {-# LANGUAGE TemplateHaskell #-}
 
 module DevourFlake.NixSystems (
-  nixSystemsPath,
+  nixSystemsFlakeFor,
 ) where
 
 import IncludeEnv.TH (includeEnv)
+import System.FilePath ((</>))
+import System.Nix.System (System (..))
 
--- | Path to github:srid/nix-systems flake output
+-- | Base path to github:srid/nix-systems flake output
 $(includeEnv "NIX_SYSTEMS_PATH" "nixSystemsPath")
 
 nixSystemsPath :: FilePath
+
+-- | Get the path to a specific system's flake under NIX_SYSTEMS_PATH
+nixSystemsFlakeFor :: System -> FilePath
+nixSystemsFlakeFor system = nixSystemsPath </> toString system
