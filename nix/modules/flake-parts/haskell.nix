33eb2a89b93ebb8b9f0923c4b6bbf96fbe3397dd
Sridhar Ratnakumar <srid@srid.ca>
2025-10-30 14:49:07 -0400

    modularize

--- nix/modules/flake-parts/haskell.nix
+++ nix/modules/flake-parts/haskell.nix
@@ -23,6 +23,9 @@ in
       ];
     };
     haskellProjects.default = { config, ... }: {
+      imports = [
+        (root + /packages/nix-cache-server/haskell-module.nix)
+      ];
       # To avoid unnecessary rebuilds, we filter projectRoot:
       # https://community.flake.parts/haskell-flake/local#rebuild
       projectRoot = lib.fileset.toSource {
@@ -49,81 +52,6 @@ in
         filepattern.source = "0.1.3";
         aeson-casing.source = "0.2.0.0";
         neat-interpolation.source = "0.5.1.4";
-
-        # nix-serve-ng as library
-        nix-serve-ng.source =
-          let
-            transformedSource = pkgs.runCommand "nix-serve-ng-transformed" { } ''
-                  cp -r ${inputs.nix-serve-ng} $out
-                  chmod -R u+w $out
-
-                  # Create NixServeNg directory
-                  mkdir -p $out/src/NixServeNg
-
-                  # Extract makeApplication to Application.hs (lines 1-319 from Main.hs)
-                  ${pkgs.gnused}/bin/sed -n '1,319p' $out/src/Main.hs > $out/src/NixServeNg/Application.hs
-
-                  # Update Application.hs: change module name
-                  ${pkgs.gnused}/bin/sed -i 's/^module Main where$/module NixServeNg.Application (\n  ApplicationOptions (..),\n  makeApplication,\n  validHashPart,\n  validHashPartBytes,\n) where/' $out/src/NixServeNg/Application.hs
-
-                  # Remove CLI-specific imports
-                  ${pkgs.gnused}/bin/sed -i '/^import Options/d' $out/src/NixServeNg/Application.hs
-                  ${pkgs.gnused}/bin/sed -i '/^import qualified Options/d' $out/src/NixServeNg/Application.hs
-                  ${pkgs.gnused}/bin/sed -i '/^import qualified System\.Environment/d' $out/src/NixServeNg/Application.hs
-                  ${pkgs.gnused}/bin/sed -i '/^import qualified Network\.Socket/d' $out/src/NixServeNg/Application.hs
-                  ${pkgs.gnused}/bin/sed -i '/^import qualified Network\.Wai\.Handler\.Warp/d' $out/src/NixServeNg/Application.hs
-                  ${pkgs.gnused}/bin/sed -i '/^import qualified Network\.Wai\.Handler\.WarpTLS/d' $out/src/NixServeNg/Application.hs
-                  ${pkgs.gnused}/bin/sed -i '/^import qualified Network\.Wai\.Middleware\.RequestLogger/d' $out/src/NixServeNg/Application.hs
-                  ${pkgs.gnused}/bin/sed -i '/^import Network\.Socket/d' $out/src/NixServeNg/Application.hs
-                  ${pkgs.gnused}/bin/sed -i '/^import Data\.Function/d' $out/src/NixServeNg/Application.hs
-                  ${pkgs.gnused}/bin/sed -i '/^import Numeric\.Natural/d' $out/src/NixServeNg/Application.hs
-
-                  # Delete CLI-specific files
-                  rm -f $out/src/Options.hs $out/src/Main.hs
-
-                  # Create NixServeNg.hs wrapper
-                  cat > $out/src/NixServeNg.hs << 'EOF'
-              {-# LANGUAGE OverloadedStrings #-}
-
-              module NixServeNg (
-                -- * Application
-                makeApplication,
-                ApplicationOptions (..),
-
-                -- * Re-exports from Nix module
-                Nix.initStore,
-                Nix.getStoreDir,
-                Nix.PathInfo (..),
-                Nix.NoSuchPath (..),
-              ) where
-
-              import NixServeNg.Application (ApplicationOptions (..), makeApplication)
-              import Nix qualified
-              EOF
-
-                  # Patch cbits/nix.cpp for newer Nix
-                  ${pkgs.gnused}/bin/sed -i 's|#include <nix/store/store-api.hh>|#include <nix/store/store-api.hh>\n    #include <nix/store/store-open.hh>|' $out/cbits/nix.cpp
-                  ${pkgs.gnused}/bin/sed -i 's|#include <nix/store/log-store.hh>|#include <nix/store/log-store.hh>\n    #include <nix/store/globals.hh>|' $out/cbits/nix.cpp
-
-                  # Transform cabal file from executable to library
-                  ${pkgs.gnused}/bin/sed -i 's/^executable nix-serve$/library/' $out/nix-serve-ng.cabal
-                  ${pkgs.gnused}/bin/sed -i 's/main-is: *Main\.hs/exposed-modules:  NixServeNg\n                    , NixServeNg.Application\n                    , Nix/' $out/nix-serve-ng.cabal
-
-                  # Remove other-modules (it's multiline: Nix, Options)
-                  ${pkgs.gnused}/bin/sed -i '/other-modules:/,/Options/d' $out/nix-serve-ng.cabal
-
-                  ${pkgs.gnused}/bin/sed -i 's/, optparse-applicative//' $out/nix-serve-ng.cabal
-                  ${pkgs.gnused}/bin/sed -i 's/cxx-options: *-std=c++20/cxx-options:      -std=c++23/' $out/nix-serve-ng.cabal
-                  ${pkgs.gnused}/bin/sed -i 's/ghc-options: *-Wall -threaded -O2 -rtsopts/ghc-options:      -Wall -O2/' $out/nix-serve-ng.cabal
-
-                  # Add ImportQualifiedPost after hs-source-dirs (inside library section)
-                  ${pkgs.gnused}/bin/sed -i '/hs-source-dirs:/a\    default-extensions: ImportQualifiedPost' $out/nix-serve-ng.cabal
-
-                  # Remove benchmark section
-                  ${pkgs.gnused}/bin/sed -i '/^benchmark benchmark/,$d' $out/nix-serve-ng.cabal
-            '';
-          in
-          transformedSource;
       };
 
       # Add your package overrides here
@@ -212,38 +140,6 @@ in
         };
         co-log-effectful.jailbreak = true;
         toml-reader.check = false;
-        nix-serve-ng = {
-          check = false;
-          custom = drv: pkgs.haskell.lib.overrideCabal drv (old: {
-            libraryHaskellDepends = (old.libraryHaskellDepends or [ ]) ++ (with pkgs.haskellPackages; [
-              base16
-              base32
-              charset
-              managed
-              http-types
-              megaparsec
-              network
-              vector
-              wai
-              wai-extra
-              warp
-              warp-tls
-            ]);
-            librarySystemDepends = (old.librarySystemDepends or [ ]) ++ [
-              pkgs.boost.dev
-              pkgs.nixVersions.latest
-            ];
-            libraryToolDepends = (old.libraryToolDepends or [ ]) ++ [ pkgs.pkg-config ];
-            libraryPkgconfigDepends = (old.libraryPkgconfigDepends or [ ]) ++ [
-              pkgs.libblake3
-              pkgs.openssl
-              pkgs.libsodium
-              pkgs.sqlite
-              pkgs.brotli
-              pkgs.curl
-            ];
-          });
-        };
       };
 
       # Development shell configuration
